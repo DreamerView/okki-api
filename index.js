f16a0b78e98982c5b6ba45a45f52d7e346628b1d
Dreamer <temirkhan.onyx@gmail.com>
2022-12-12 22:26:12 +0600

    ssd

--- index.js
+++ index.js
@@ -1,16 +1,78 @@
+// Configuration
+require('dotenv').config()
+const mode = process.env.MODE; //(Choose: pro/dev) pro - producation or dev - development
+const PORT = process.env.PORT || 3001;
+const connectSocket = "http://localhost:3000";
+const IpHostList = mode === "dev"?['localhost']:['okki.kz','zhenil-next.vercel.app'];
+//
+
+// Library 
 const express = require('express');
 const app = express();
 const http = require('http');
 const {Server} = require("socket.io");
-
 const server = http.createServer(app);
+const cors = require('cors');
+const modeRes1 = mode==="dev"?"localhost":"zhenil-next.vercel.app"
+const modeRes2 = mode==="dev"?"localhost":"okki.kz"
+const helmet = require('helmet');
+const {v4: uuidv4} = require('uuid');
+// console.log(uuidv4());
+//
 
-app.get('/', (req, res) => {
+
+const generatePassword = () => {
+    var length = 64,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+};
+
+console.log(generatePassword());
+
+var corsOptions = {
+    // origin: connectSocket,
+    allowedHeaders: 'Content-Type,Authorization,Accept',
+    methods: "GET,POST",
+    optionsSuccessStatus: 200,
+    credentials:true
+}
+app.use((req, res, next) => {
+    let validHost = IpHostList; // Put your IP whitelist in this array
+    
+    if(validHost.includes(req.hostname)){
+        console.log("Host ok");
+        next();
+    } else{
+        console.log("Bad host: " + req.hostname);
+        // const err = new Error("Bad host: " + req.connection.remoteAddress);
+        // next(err);
+        res.status(401).send("<h1>Access denied!</h1><p>Please exit this page!</p>")
+    }
+})
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use(cors(corsOptions));
+app.use(helmet());
+app.disable('x-powered-by');
+
+// Router start
+const database = require('./pages/user/index');
+app.use(database);
+
+// Router finish
+
+app.route('/').get((req, res) => {
+    // const s = require("./pages/user/index.js")
     res.send(`
         <h1>Welcome to Okki Api Server</h1>
         `);
 });
-app.get('/ws', (req, res) => {
+app.route('/ws').get((req, res) => {
     res.send(`
         <h1>Welcome to Okki WebSocket</h1>
         `);
@@ -18,7 +80,7 @@ app.get('/ws', (req, res) => {
 
 const io = new Server(server,{
     cors: {
-        origin: "http://localhost:3000",
+        origin: connectSocket,
         methods: ["GET", "POST"],
         transports: ['websocket', 'polling'],
         credentials: true
@@ -39,8 +101,17 @@ io.of("/ws").on('connection',socket=>{
             socket.broadcast.to(rooms).emit("get_question",e);
         })
     }
-})
+});
 
-server.listen(3001,()=>{
-    console.log("Server is running");
+app.use((req, res, next) => {
+    res.status(404).send("<h1>404 Error</h1><p>Sorry can't find that!</p>")
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('<h1>500 Error</h1><p>Something broke!</p>')
+});
+
+server.listen(PORT,()=>{
+    console.log("Server is running on port "+PORT);
 })
---
+++ pages/user/index.js
@@ -0,0 +1,105 @@
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const router = express.Router();
+const knex = require('knex')({
+    client: 'mysql2',
+    connection: {
+      host : process.env.DATABASE_HOST,
+      port : process.env.DATABASE_PORT,
+      user : process.env.DATABASE_USER,
+      password : process.env.DATABASE_PASSWORD,
+      database : 'users'
+    }
+});
+const nodemailer = require('nodemailer');
+let transporter = nodemailer.createTransport({
+    host: 'mail.okki.kz',
+    port:993,
+    secure:true,
+    auth: {
+        user: "support@okki.kz",
+        pass: "!M2raumOp"
+    }
+})
+const AesEncryption = require('aes-encryption');
+const aes = new AesEncryption();
+aes.setSecretKey(process.env.AES_KEY);
+
+const authToken = (req,res,next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1];
+    if(token==null) return res.sendStatus(401);
+
+    jwt.verify(token,process.env.ACCESS_TOKEN,(err,uid)=>{
+        if(err) return res.sendStatus(403);
+        req.uid = uid;
+        next()
+    })
+};
+
+router.get('/forget',(req,res)=>{
+    let message = {
+        from: '"Node js" <support@okki.kz>',
+        to: "temirkhan.onyx@gmail.com",
+        subject: "Subject",
+        text: "Hello SMTP Email"
+    }
+    transporter.sendMail(message,function(err, info) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log(info);
+        }
+    });
+    res.send("Forget password")
+})
+
+
+router.get('/database-select',(req,res)=>{
+    // console.log(router);
+    // knex.raw("SELECT 1").then(() => {
+    //     return "Connected"
+    // }).catch(e=>{return "error"})
+    let select_array = req.query.select!==undefined?req.query.select:"";
+    knex.select(select_array).from("users").then(e=>{
+        res.json(e)
+    }).catch(e=>{res.send(e)});
+});
+
+router.post('/database-insert', (req, res) => {    
+    console.log(req.body);  
+    res.json(req.body);  
+});
+
+router.get("/select1",authToken,(req,res)=>{
+    res.json("Yes you did it!");
+})
+
+router.post('/login', async(req, res) => {
+    const uid =  aes.decrypt(req.body.email);
+    const pass = aes.decrypt(req.body.password);
+    console.log(uid);
+    try {
+        if(uid!==undefined) {
+            const start = await knex.select("uuid","name","surname").where({email:uid}).andWhere({password:pass}).from("users")
+            if(start.length == 0) {
+                console.log("Not found");
+                res.sendStatus(404);
+            } else {
+                start.map(result=>{
+                    const AccessToken = jwt.sign({uid:result.uuid},process.env.ACCESS_TOKEN,{ expiresIn: '30s' });
+                    res.json({accessToken:aes.encrypt(AccessToken),name:aes.encrypt(result.name),surname:aes.encrypt(result.surname)});
+                    console.log("Exist: "+uid);
+                    console.log("New token to "+uid+" is: "+AccessToken);
+                });
+            }
+        }
+    } catch {
+        res.sendStatus(500);
+    }
+});
+router.post('/token',(req,res) => {
+
+})
+
+module.exports = router;
