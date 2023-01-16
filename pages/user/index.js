cf2a9d3e6458d64368aac01f0617aed66efb50cd
Dreamer <temirkhan.onyx@gmail.com>
2023-01-16 08:51:37 +0600

    dsadsaadsasd

--- pages/user/index.js
+++ pages/user/index.js
@@ -43,10 +43,12 @@ router.get('/get-devices',authToken,async(req,res)=>{
         const uuid = req.uid.uuid;
         console.time("/get-devices finished with");
         if(uuid!==undefined && uuid!==null) {
-            const result = await knex(uuid+"_usersToken").select('clientId','clientInfo','getTime','ipInfo');
-            res.status(200).json({clientId:req.uid.clientId,result:result});
+            const result  = await knex.raw("select `clientId`,`clientInfo`,`getTime`,`ipInfo` from `"+uuid+"_usersToken`");
+            // console.log(result[0])
+            // const result = await knex(uuid+"_usersToken").select('clientId','clientInfo','getTime','ipInfo');
+            console.timeEnd("/get-devices finished with");
+            return res.status(200).json({clientId:req.uid.clientId,result:result[0]});
         } else return timerStart(res.sendStatus(406));
-        console.timeEnd("/get-devices finished with");
     } catch(e) {
         console.log('\x1b[31m%s\x1b[0m',"/get-devices - Mistake, mistake is ");
         console.log(e);
@@ -60,9 +62,10 @@ router.get('/get-data',authToken,async(req,res)=>{
         const uuid = req.uid.uuid;
         if(uuid!==undefined && uuid!==null) {
             let cryptoKey,nameUser,surnameUser,dataUser,avatarUser,loginUser,clientUser;
-            const getCrypto = await knex("usersKey").select('keyCrypto').where({uuid:uuid}),getDatabase = await knex("users").select("name","surname","data","avatar","login","client").where("uuid",uuid);
-            getCrypto.map(result=>cryptoKey=result.keyCrypto);
-            getDatabase.map(result=>{nameUser=aes256({key:cryptoKey,method:"dec",text:result.name});surnameUser=aes256({key:cryptoKey,method:"dec",text:result.surname});dataUser=result.data;avatarUser=result.avatar;clientUser=result.client;loginUser=aes256({key:cryptoKey,method:"dec",text:result.login});});
+            const getCrypto = await knex.raw("select `keyCrypto` from `usersKey`"),getDatabase = await knex.raw('select `name`,`surname`,`data`,`avatar`,`login`,`client` from `users` where uuid="'+uuid+'"');
+            // const getCrypto = await knex("usersKey").select('keyCrypto').where({uuid:uuid}),getDatabase = await knex("users").select("name","surname","data","avatar","login","client").where("uuid",uuid);
+            getCrypto[0].map(result=>cryptoKey=result.keyCrypto);
+            getDatabase[0].map(result=>{nameUser=aes256({key:cryptoKey,method:"dec",text:result.name});surnameUser=aes256({key:cryptoKey,method:"dec",text:result.surname});dataUser=result.data;avatarUser=result.avatar;clientUser=result.client;loginUser=aes256({key:cryptoKey,method:"dec",text:result.login});});
             const httpCheck = req.hostname==='localhost'?'http://':"https://",portCheck = req.hostname==='localhost'?':'+process.env.PORT:"",avatarResult = clientUser==="okki"?httpCheck+req.hostname+portCheck+avatarUser:avatarUser;
             res.status(200).json({name:aes.encrypt(nameUser),surname:aes.encrypt(surnameUser),data:aes.encrypt(dataUser),avatar:aes.encrypt(avatarResult),login:aes.encrypt(loginUser)});
         } else return timerStart(res.sendStatus(406));
